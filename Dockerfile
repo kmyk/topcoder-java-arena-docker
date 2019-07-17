6eba1728be4ec83fe542b9e2ff1a176136e748ca
Kimiyuki Onaka <kimiyuki95@gmail.com>
2019-07-17 12:27:11 +0900

    add configuration files and install Greed

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,7 @@ RUN apt-get update && apt-get install -y \
     && rm -rf /var/lib/apt/lists/*
 
 # RUN wget http://www.topcoder.com/contest/arena/ContestAppletProd7.2.jnlp
-RUN wget http://www.topcoder.com/contest/arena/ContestAppletProd.jnlp
+RUN wget http://www.topcoder.com/contest/arena/ContestAppletProd.jnlp \
+    && wget https://github.com/zenozen/topcoder-greed/releases/download/2.0-RC/Greed-2.0-RC-7.1.0.jar
 
 CMD ["javaws", "/ContestAppletProd.jnlp"]
---
+++ template.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for (int i = 0; (i) < (int)(n); ++ (i))
+#define REP3(i, m, n) for (int i = (m); (i) < (int)(n); ++ (i))
+#define REP_R(i, n) for (int i = (int)(n) - 1; (i) >= 0; -- (i))
+#define REP3R(i, m, n) for (int i = (int)(n) - 1; (i) >= (int)(m); -- (i))
+#define ALL(x) begin(x), end(x)
+using namespace std;
+class ${ClassName} { public: ${Method.ReturnType} ${Method.Name}(${Method.Params}); };
+
+${Method.ReturnType} ${ClassName}::${Method.Name}(${Method.Params}) {
+    return ${Method.ReturnType;zeroval};
+}
+
+${CutBegin}
+#if 1
+${<TestCode}
+#endif
+${CutEnd}
